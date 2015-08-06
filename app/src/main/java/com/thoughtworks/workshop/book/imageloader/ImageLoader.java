76ca2cfe3617056080a739243b479581641badb0
gongming <gongmingqm10@foxmail.com>
2015-08-06 09:01:58 +0800

    [Ming] - 2nd-step2 - Do not mess the book images

--- app/src/main/java/com/thoughtworks/workshop/book/imageloader/ImageLoader.java
+++ app/src/main/java/com/thoughtworks/workshop/book/imageloader/ImageLoader.java
@@ -2,10 +2,13 @@ package com.thoughtworks.workshop.book.imageloader;
 
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
+import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
 import android.text.TextUtils;
 import android.widget.ImageView;
+
+import com.thoughtworks.workshop.book.R;
 
 import java.io.IOException;
 import java.net.URL;
@@ -15,6 +18,8 @@ public class ImageLoader {
     private static final int IMAGE_LOAD_MESSAGE = 100;
     private ImageView currentImageView;
 
+    private final String KEY_URL = "url";
+    private final String KEY_BITMAP = "bitmap";
 
     private static ImageLoader instance;
 
@@ -30,7 +35,7 @@ public class ImageLoader {
 
     public void loadImage(final ImageView imageView, final String urlString) {
 
-        if (TextUtils.isEmpty(urlString)) return;
+        if (TextUtils.isEmpty(urlString) || imageView == null) return;
 
         new Thread(new Runnable() {
             @Override
@@ -39,12 +44,18 @@ public class ImageLoader {
                     URL url = new URL(urlString);
                     Bitmap imageBitmap = BitmapFactory.decodeStream(url.openConnection().getInputStream());
 
+                    if (imageBitmap == null) return;
 
                     currentImageView = imageView;
+                    currentImageView.setTag(urlString);
+
+                    Bundle bundle = new Bundle();
+                    bundle.putString(KEY_URL, urlString);
+                    bundle.putParcelable(KEY_BITMAP, imageBitmap);
 
                     Message message = new Message();
                     message.what = IMAGE_LOAD_MESSAGE;
-                    message.obj = imageBitmap;
+                    message.setData(bundle);
                     handler.sendMessage(message);
 
                 } catch (IOException e) {
@@ -52,14 +63,18 @@ public class ImageLoader {
                 }
             }
         }).start();
-
     }
 
     private Handler handler = new Handler() {
         @Override
         public void dispatchMessage(Message msg) {
-            if (msg.what == IMAGE_LOAD_MESSAGE && msg.obj != null) {
-                currentImageView.setImageBitmap((Bitmap)msg.obj);
+            Bundle data = msg.getData();
+            if (msg.what == IMAGE_LOAD_MESSAGE && data != null) {
+                if (currentImageView.getTag().equals(data.getString(KEY_URL))) {
+                    currentImageView.setImageBitmap((Bitmap)data.getParcelable(KEY_BITMAP));
+                } else {
+                    currentImageView.setImageResource( R.drawable.ic_default_cover);
+                }
             }
         }
     };
